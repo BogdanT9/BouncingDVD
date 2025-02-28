70d7b651820a5eebecc3b327db56985cccbd7241
BogdanT9 <bogdan.tache@stud.acs.upb.ro>
2025-02-28 12:52:24 +0200

    Implemented closing

--- src/app.h
+++ src/app.h
@@ -8,3 +8,10 @@
 
 #define SCREEN_WIDTH 720
 #define SCREEN_HEIGHT 450
+
+void init();
+void setup();
+void handle_input();
+void update();
+void draw();
+void close();
--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 SDL_Renderer* renderer = NULL;
+bool running = true;
 
-void setup() {
+void init() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
         exit(1);
@@ -27,7 +28,45 @@ void setup() {
     }
 }
 
+void setup() {}
+
+void handle_input() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                running = false;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void update() {}
+
+void draw() {}
+
+void close() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindowSurface(window);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main() {
+    init();
     setup();
+    while (running) {
+        handle_input();
+        update();
+        draw();
+    }
+    close();
     return 0;
 }
